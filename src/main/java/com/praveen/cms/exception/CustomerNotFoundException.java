62d5ecec69ce7d96e22c6cf0b185bd7c151f0f4b
PraveenYadav23 <praveencs0042@gmail.com>
2020-03-09 16:15:25 +0530

    JWT security is added

--- src/main/java/com/praveen/cms/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.praveen.cms.exception;
-
-public class CustomerNotFoundException extends AppException {
-
-    public CustomerNotFoundException(String errorType, String errorCode, String errorMessage) {
-        super(errorType, errorCode, errorMessage);
-    }
-
-}
